3924c9e85142fd32503d87ce755181e7f27fa8a7
Sang Yeong Lee <46237147+Sangyeonglee353@users.noreply.github.com>
2023-02-03 23:51:30 +0900

    Update HeaderCartButton

--- react-food-order-app/src/components/Layout/Header.js
+++ react-food-order-app/src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React, {Fragment} from 'react';
 import classes from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
     return (
         <Fragment>
             <header className={classes.header}>
-                <h2>ReactMeals</h2>
-                <button>Cart</button>
+                <h1>ReactMeals</h1>
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt='A table full of delicious food!'/>
