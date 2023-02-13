0d4e8d1647563ac2d3f07b5c586b29f10a5f4f6f
Sang Yeong Lee <46237147+Sangyeonglee353@users.noreply.github.com>
2023-02-13 21:25:42 +0900

    Update MindMap component

--- how-about-idea/src/components/MindMap/Mind.js
+++ how-about-idea/src/components/MindMap/Mind.js
@@ -1,9 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import MindMap from "./MindMap";
 import * as go from "./go.js";
-
-// cdn
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gojs/1.7.15/go.js" integrity="sha512-kXHN1AOrS2dtCHVW78WNft0tHWfScHTivmPRZVmY9uKF8DEWP9pT5gBLoyAGvNVg6f6GK2u3fPQkW/up2aZisA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 const MindCss = styled.div`
   /* width:100vw;
@@ -14,125 +12,45 @@ height:100vh; */
     width: 100%;
     height: 500px;
     border: solid 1px black;
-    background-color: white;
+    background-color: blue;
   }
 `;
 
 /* Go.js */
-// function initDiagram() {
-//     const $ = go.GraphObject.make;
-//     // set your license key here before creating the diagram: go.Diagram.licenseKey = "...";
-//     const diagram =
-//       $(go.Diagram,
-//         {
-//           'undoManager.isEnabled': true,  // must be set to allow for model change listening
-//           // 'undoManager.maxHistoryLength': 0,  // uncomment disable undo/redo functionality
-//           'clickCreatingTool.archetypeNodeData': { text: 'new node', color: 'lightblue' },
-//           model: new go.GraphLinksModel(
-//             {
-//               linkKeyProperty: 'key'  // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel
-//             })
-//         });
-
-//     // define a simple Node template
-//     diagram.nodeTemplate =
-//       $(go.Node, 'Auto',  // the Shape will go around the TextBlock
-//         new go.Binding('location', 'loc', go.Point.parse).makeTwoWay(go.Point.stringify),
-//         $(go.Shape, 'RoundedRectangle',
-//           { name: 'SHAPE', fill: 'white', strokeWidth: 0 },
-//           // Shape.fill is bound to Node.data.color
-//           new go.Binding('fill', 'color')),
-//         $(go.TextBlock,
-//           { margin: 8, editable: true },  // some room around the text
-//           new go.Binding('text').makeTwoWay()
-//         )
-//       );
-
-//     return diagram;
-//   }
-
 /**
  * This function handles any changes to the GoJS model.
  * It is here that you would make any updates to your React state, which is dicussed below.
  */
-function handleModelChange(changes) {
-  console.log("GoJS model changed!");
-}
-
-// var diagram = new go.Diagram("myDiagramDiv");
-// Sample 1
-//   diagram.model = new go.GraphLinksModel(
-//     [{ key: "Hello" },   // two node data, in an Array
-//      { key: "World!" }],
-//     [{ from: "Hello", to: "World!"}]  // one link data, in an Array
-//   );
-
-// Sample 2
-// the node template describes how each Node should be constructed
-// diagram.nodeTemplate = new go.Node("Auto") // the Shape will go around the TextBlock
-//   .add(
-//     new go.Shape("RoundedRectangle")
-//       // Shape.fill is bound to Node.data.color
-//       .bind("fill", "color")
-//   )
-//   .add(
-//     new go.TextBlock({ margin: 8 }) // Specify a margin to add some room around the text
-//       // TextBlock.text is bound to Node.data.key
-//       .bind("text", "key")
-//   );
-
-// // the Model holds only the essential information describing the diagram
-// diagram.model = new go.GraphLinksModel(
-//   [
-//     // a JavaScript Array of JavaScript objects, one per node;
-//     // the "color" property is added specifically for this app
-//     { key: "Alpha", color: "lightblue" },
-//     { key: "Beta", color: "orange" },
-//     { key: "Gamma", color: "lightgreen" },
-//     { key: "Delta", color: "pink" },
-//   ],
-//   [
-//     // a JavaScript Array of JavaScript objects, one per link
-//     { from: "Alpha", to: "Beta" },
-//     { from: "Alpha", to: "Gamma" },
-//     { from: "Beta", to: "Beta" },
-//     { from: "Gamma", to: "Delta" },
-//     { from: "Delta", to: "Alpha" },
-//   ]
-// );
-// // enable Ctrl-Z to undo and Ctrl-Y to redo
-// diagram.undoManager.isEnabled = true;
-
-// Sample 3
-let nodeDataArray = [
-  { key: 0, text: "Alpha", color: "lightblue", loc: "0 0" },
-  { key: 1, text: "Beta", color: "orange", loc: "150 0" },
-  { key: 2, text: "Gamma", color: "lightgreen", loc: "0 150" },
-  { key: 3, text: "Delta", color: "pink", loc: "150 150" },
-];
-
-let linkDataArray = [
-  { key: -1, from: 0, to: 1 },
-  { key: -2, from: 0, to: 2 },
-  { key: -3, from: 1, to: 1 },
-  { key: -4, from: 2, to: 3 },
-  { key: -5, from: 3, to: 0 },
-];
 
 function initDiagram() {
+  // 1. Set diagram property in "myDiagramDiv"
   const $ = go.GraphObject.make;
-  // set your license key here before creating the diagram: go.Diagram.licenseKey = "...";
   const diagram = $(go.Diagram, "myDiagramDiv", {
-    "undoManager.isEnabled": true, // must be set to allow for model change listening
-    // 'undoManager.maxHistoryLength': 0,  // uncomment disable undo/redo functionality
+    // 1.1 enable undo & redo
+    "undoManager.isEnabled": true,
+
+    // 1.2 uncomment disable undo/redo functionality
+    // 'undoManager.maxHistoryLength': 0,
+
+    // 1.3 allow double-click in background to create a new node
     "clickCreatingTool.archetypeNodeData": {
       text: "new node",
       color: "lightblue",
     },
-    model: new go.GraphLinksModel(nodeDataArray, linkDataArray),
+
+    // 1.4 hav mouse whell events zoom in and out instead of scroll up and down
+    "toolManager.mouseWheelBehavior": go.ToolManager.WheelZoom,
+
+    // 1.5 an initial automatic zoom-to-fit
+    initialAutoScale: go.Diagram.Uniform,
+
+    // 1.6 align document to the center of the viewport
+    contentAlignment: go.Spot.Center,
+
+    // model: new go.GraphLinksModel(nodeDataArray, linkDataArray),
   });
 
-  // define a simple Node template
+  // 2. Define a simple Node template: Diagram CSS
   diagram.nodeTemplate = $(
     go.Node,
     "Auto", // the Shape will go around the TextBlock
@@ -142,7 +60,20 @@ function initDiagram() {
     $(
       go.Shape,
       "RoundedRectangle",
-      { name: "SHAPE", fill: "white", strokeWidth: 0 },
+      {
+        name: "SHAPE",
+        fill: "white",
+        strokeWidth: 0,
+        stroke: null,
+        portId: "",
+        cursor: "pointer",
+        fromLinkable: true,
+        fromLinkableSelfNode: true,
+        fromLinkableDuplicates: true,
+        toLinkable: true,
+        toLinkableSelfNode: true,
+        toLinkableDuplicates: true,
+      },
       // Shape.fill is bound to Node.data.color
       new go.Binding("fill", "color")
     ),
@@ -152,14 +83,71 @@ function initDiagram() {
       new go.Binding("text").makeTwoWay()
     )
   );
+
+  diagram.linkTemplate = $(
+    go.Link,
+    {
+      // allow the user to reconnnect existing links:
+      relinkableFrom: true,
+      relinkableTo: true,
+      // draw the link path shorter than normal,
+      // so that it does not interfere with the appearance of the arrowhead
+      toShortLength: 2,
+    },
+    $(go.Shape, { strokeWidth: 2 }),
+    $(go.Shape, { toArrow: "Standard", stroke: null })
+  );
+
+  // // 3. Create diagram
+  // // 3.1 Static type
+  // // 심플하게 nodeDataArray에 값이 추가된 경우 해당 함수를 useEffect를 통해 다시 한 번 실행하면 되지 않는가?
+  // // 값이 들어오는 경우는 세 가지 이다.
+  // // (1) nodeDataArray만 추가되는 경우
+  // // (2) linkDataArray만 추가되는 경우
+  // // (3) nodeDataArray와 linkDataArray 둘 다 추가되는 경우
+  // diagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
+
+  return diagram;
 }
 
 function Mind() {
-  useEffect(initDiagram, []);
-  
+  let DUMMY_nodeDataArray = [
+    { key: 0, text: "Alpha", color: "lightblue", loc: "0 0" },
+    { key: 1, text: "Beta", color: "orange", loc: "150 0" },
+    { key: 2, text: "Gamma", color: "lightgreen", loc: "0 150" },
+    { key: 3, text: "Delta", color: "pink", loc: "150 150" },
+  ];
+
+  let DUMMY_linkDataArray = [
+    { key: -1, from: 0, to: 1 },
+    { key: -2, from: 0, to: 2 },
+    { key: -3, from: 1, to: 1 },
+    { key: -4, from: 2, to: 3 },
+    { key: -5, from: 3, to: 0 },
+  ];
+  const [nodeDataArray, setNodeDataArray] = useState(DUMMY_nodeDataArray);
+  const [linkDataArray, setLinkDataArray] = useState(DUMMY_linkDataArray);
+
+  // const addNodeDataHandler = (props) => {
+
+  // };
+
+  // const addLinkDataHandler = (props) => {
+
+  // };
+
+  // useEffect(initDiagram, []);
+
   return (
     <MindCss>
-      <div id="myDiagramDiv" className="diagram-component" />
+      {/* <div id="myDiagramDiv" className="diagram-component" /> */}
+      <MindMap
+        initDiagram={initDiagram}
+        divClassName="diagram-component"
+        nodeDataArray={nodeDataArray}
+        linkDataArray={linkDataArray}
+      />
+      <p>mindMap test</p>
     </MindCss>
   );
 }
