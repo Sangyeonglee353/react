0c9e5ede0b28e1d686580e0dc980c1b3e739f83c
Sang Yeong Lee <46237147+Sangyeonglee353@users.noreply.github.com>
2023-03-30 00:15:32 +0900

    Update capture function

--- capture-app/src/App.js
+++ capture-app/src/App.js
@@ -1,13 +1,49 @@
-import logo from "./logo.svg";
-import "./App.css";
+import html2canvas from "html2canvas";
+import styled from "styled-components";
 
+const AppCSS = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  #temp {
+    width: 300px;
+    height: 300px;
+    border: 1px solid black;
+  }
+  button {
+    margin-top: 10px;
+    width: 100px;
+    height: 20px;
+  }
+`;
+
+const onCapture = () => {
+  html2canvas(document.querySelector("#temp"), {}).then((canvas) => {
+    saveAs(canvas.toDataURL(), "이름.png");
+  });
+};
+
+const saveAs = (uri, filename) => {
+  let link = document.createElement("a");
+  if (typeof link.download === "string") {
+    link.href = uri;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  } else {
+    window.open(uri);
+  }
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <button>Capture</button>
-      </header>
-    </div>
+    <AppCSS>
+      <div id="temp">Test Capture</div>
+      <button onClick={onCapture}>Capture</button>
+    </AppCSS>
   );
 }
 
