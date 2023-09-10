7e9410e9d3ad2fe4b49e94df55f38d847d5e3e6f
Sang Yeong Lee <Sangyeong.lee353@gmail.com>
2023-09-10 18:26:21 +0900

    update react-tictactoe-app func & design

--- react-tictactoe-app/src/App.js
+++ react-tictactoe-app/src/App.js
@@ -1,18 +1,88 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Board from "./components/Board";
+import "./App.css";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board"></div>
-        <div className="game-info">
-          <div>
-            <Board />
-          </div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
+const App = () => {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0);
+  const current = history[stepNumber];
+
+  // 승자 계산
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = calculateWinner(current.squares);
+  let status;
+  if (winner) {
+    status = "Winnder: " + winner;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
   }
-}
+
+  const handleClick = (i) => {
+    const newHistory = history.slice(0, stepNumber + 1);
+    const newCurrent = history[history.length - 1];
+    const newSquares = current.squares.slice();
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? "X" : "O";
+    setHistory([...newHistory, { squares: newSquares }]);
+    setXIsNext((prev) => !prev);
+
+    setStepNumber(newHistory.length);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button className="move-button" onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-title">Tic Tac Toe</div>
+      <div className="game-board">
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        <div className="status">{status}</div>
+        <ol style={{ listStyle: "none" }}>{moves}</ol>
+      </div>
+    </div>
+  );
+};
+
+export default App;
