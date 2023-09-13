41782a59411fb9b7fd57fb117a1d777ae6f68c66
Sang Yeong Lee <Sangyeong.lee353@gmail.com>
2023-09-13 20:44:31 +0900

    Update Nav and Banner, Category

--- react-disney-plus-app/src/App.js
+++ react-disney-plus-app/src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
+import Nav from "./components/Nav";
+import styled from "styled-components";
 
+const Containner = styled.main`
+position: relative;
+min-height; calc(100vh - 250px);
+overflow-x: hidden;
+display: block;
+top: 72px;
+padding: 0 calc(3.5vw + 5px);
+
+&:after {
+  background: url("/images/home-background.png") center center / cover no-repeat fixed;
+  content: "";
+  position: absolute;
+  inset: 0px;
+  opacity: 1;
+  z-index: -1;
+}
+
+`;
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Containner>
+      <Nav />
+    </Containner>
+  );
 };
 
 export default App;
